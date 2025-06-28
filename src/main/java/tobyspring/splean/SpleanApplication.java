3de258d65037d6ce409926e20d741b976c7099aa
HongYouBin <ghddbqls33@gmail.com>
2025-06-28 23:41:16 +0900

    first commit

---
+++ src/main/java/tobyspring/splean/SpleanApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.splean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tobyspring/splean/SpleanApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.splean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
