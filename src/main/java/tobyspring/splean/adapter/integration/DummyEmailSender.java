4d317750d2d746a095f4253d5f90d059c63a06cf
HongYouBin <ghddbqls33@gmail.com>
2025-07-14 00:17:50 +0900

    password encoder 추가

---
+++ src/main/java/tobyspring/splean/adapter/integration/DummyEmailSender.java
@@ -0,0 +1,15 @@
+package tobyspring.splean.adapter.integration;
+
+import org.springframework.context.annotation.Fallback;
+import org.springframework.stereotype.Component;
+import tobyspring.splean.application.required.EmailSender;
+import tobyspring.splean.domain.Email;
+
+@Component
+@Fallback
+public class DummyEmailSender implements EmailSender {
+    @Override
+    public void send(Email email, String subject, String body) {
+        System.out.println("dummy email send : " + email);
+    }
+}
---
+++ src/main/java/tobyspring/splean/adapter/security/SecurityPasswordEncoder.java
@@ -0,0 +1,20 @@
+package tobyspring.splean.adapter.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import tobyspring.splean.domain.PasswordEncoder;
+
+@Component
+public class SecurityPasswordEncoder implements PasswordEncoder {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public String encode(String password) {
+        return bCryptPasswordEncoder.encode(password);
+    }
+
+    @Override
+    public boolean matches(String password, String passwordHash) {
+        return bCryptPasswordEncoder.matches(password, passwordHash);
+    }
+}
